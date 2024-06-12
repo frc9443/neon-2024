bfee419460c6e4458e3b6b03cee05c882ffe8ae4
David J Parrott <valthon@nothlav.net>
2024-06-12 08:30:13 -0400

    Fix pneumatics to use correct CAN Id for the hub

--- src/main/java/frc/robot/subsystems/pneumatics/PneumaticsConstants.java
+++ src/main/java/frc/robot/subsystems/pneumatics/PneumaticsConstants.java
@@ -1,6 +1,6 @@
 package frc.robot.subsystems.pneumatics;
 
 public class PneumaticsConstants {
-    public static final int PneumaticHubCanId = 21;
+    public static final int PneumaticHubCanId = 20;
     public static final int SolenoidId = 1;
 }
